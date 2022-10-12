68e080a3fe1bf83d4171e68679c07e778d36e6e1
Kevin Anaya <kevinanaya278@gmail.com>
2022-10-12 11:18:46 -0500

    agregamos el chrome driver, creamos el metodo before en nuestro stepdefinitions para la configuracion de actores y iniciamos el primer metodo que es el Given

--- src/test/java/com/co/utest/stepsdefinitions/RegisterStepDefinition.java
+++ src/test/java/com/co/utest/stepsdefinitions/RegisterStepDefinition.java
@@ -1,17 +1,33 @@
 package com.co.utest.stepsdefinitions;
 
+import cucumber.api.java.Before;
 import cucumber.api.java.en.Given;
 import cucumber.api.java.en.Then;
 import cucumber.api.java.en.When;
+import net.serenitybdd.screenplay.abilities.BrowseTheWeb;
+import net.serenitybdd.screenplay.actions.Open;
+import net.serenitybdd.screenplay.actors.Cast;
+import net.serenitybdd.screenplay.actors.OnStage;
+import net.thucydides.core.annotations.Managed;
+import org.openqa.selenium.WebDriver;
 
 public class RegisterStepDefinition {
 
     //utilizamos el chromedriver
+    @Managed
+    WebDriver hisBrowser;
 
+    @Before
+    public void setUp(){
+        OnStage.setTheStage((Cast.ofStandardActors()));
+        OnStage.theActorCalled("Kevin");
+        OnStage.theActorInTheSpotlight().can(BrowseTheWeb.with(hisBrowser));
+    }
 
 
     @Given("^he user is in the web page$")
     public void heUserIsInTheWebPage() {
+        OnStage.theActorInTheSpotlight().wasAbleTo(Open.url("https://utest.com/"));
     }
 
 
